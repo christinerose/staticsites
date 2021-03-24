d04d771f5ed1a0ddfaf0df529df0f7a05287dc05
Christine Rose <professor.rose@gmail.com>
2021-03-24 15:18:05 -0700

    trying to figure out bug

--- my-gatsby-project/gatsby-config.js
+++ my-gatsby-project/gatsby-config.js
@@ -4,9 +4,22 @@ module.exports = {
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
+
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+
+   /*added*/ 
+   [`gatsby-plugin-sass`],
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        implementation: require("sass"),
+      },
+    },
+
+
+
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
